7617bbbcf8ac9029df3351bf67de4442cd072d77
Jad El Jakhlab <jadjakhlab@gmail.com>
2026-06-12 01:41:16 +0200

    Improve MS4 release prototype UI

--- app/src/main/java/com/example/bafoegbuddy/DashboardActivity.kt
+++ app/src/main/java/com/example/bafoegbuddy/DashboardActivity.kt
@@ -2,6 +2,7 @@ package com.example.bafoegbuddy
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,27 +11,27 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
+        val applicationCard = findViewById<View>(R.id.applicationCard)
+        applicationCard.setOnClickListener {
+            val intent = Intent(this, ApplicationActivity::class.java)
+            startActivity(intent)
+        }
+
         val documentsCard = findViewById<TextView>(R.id.documentsCard)
         documentsCard.setOnClickListener {
             val intent = Intent(this, DocumentsActivity::class.java)
             startActivity(intent)
         }
 
-        val followUpsCard = findViewById<TextView>(R.id.followUpsCard)
-        followUpsCard.setOnClickListener {
-            val intent = Intent(this, FollowUpsActivity::class.java)
-            startActivity(intent)
-        }
-
-        val applicationCard = findViewById<TextView>(R.id.applicationCard)
-        applicationCard.setOnClickListener {
-            val intent = Intent(this, ApplicationActivity::class.java)
-            startActivity(intent)
-        }
-
         val deadlinesCard = findViewById<TextView>(R.id.deadlinesCard)
         deadlinesCard.setOnClickListener {
             val intent = Intent(this, DeadlinesActivity::class.java)
+            startActivity(intent)
+        }
+
+        val followUpsCard = findViewById<TextView>(R.id.followUpsCard)
+        followUpsCard.setOnClickListener {
+            val intent = Intent(this, FollowUpsActivity::class.java)
             startActivity(intent)
         }
     }
