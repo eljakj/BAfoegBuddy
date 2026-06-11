33699905407f5b5a356abd8839f345258ce1020c
Jad El Jakhlab <jadjakhlab@gmail.com>
2026-06-12 00:19:48 +0200

    Add dashboard screen and navigation

--- app/src/main/java/com/example/bafoegbuddy/MainActivity.kt
+++ app/src/main/java/com/example/bafoegbuddy/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.bafoegbuddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val startButton = findViewById<Button>(R.id.startButton)
+        startButton.setOnClickListener {
+            val intent = Intent(this, DashboardActivity::class.java)
+            startActivity(intent)
         }
     }
 }
